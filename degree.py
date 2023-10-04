7bea999f8dbd50d4463adc6d56ab24bbd8ade674
roganofff <matematic1337@gmail.com>
2023-10-04 14:12:14 +0300

    Docstrings are done

--- degree.py
+++ degree.py
@@ -1,2 +1,23 @@
-def dergee(radius: float, time: float, acceleration: float, velocity: float = 0) -> float:
-    
+""""Module for calculating the rotation of the ball in degrees."""
+
+from math import pi
+
+
+def degree(radius: float, time: float, acceleration: float, velocity: float = 0) -> float:
+    """Calculate degree of rotation of the ball from the starting position.
+
+    Args:
+        radius: float - radius of the ball.
+        time: float - the time in which the ball moves .
+        acceleration: float - accelerating the ball.
+        velocity: float - the speed at which the ball moves.
+
+    Returns:
+        degree of rotation of the ball.
+
+    Raises:
+        Exeption - if rate is not defined for position (position unknow)."""
+    distance = velocity * time + (acceleration * time ** 2) / 2
+    length = 2 * pi * radius
+    new_degree = ((distance % length) / length) * 360
+    return round(new_degree, 2)
